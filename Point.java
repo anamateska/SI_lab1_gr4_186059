59303e3de9d0a16a957ebdb8e458536eccfa5b0f
anamateska <63292673+anamateska@users.noreply.github.com>
2020-04-07 22:52:45 +0200

    color variable added

---
+++ Point.java
@@ -0,0 +1,23 @@
+class Point {
+
+	String id
+	double x,y;
+
+	//TODO add new variable
+	String boja;
+
+	//TODO constructor
+
+	//TODO setters and getters
+
+	public void move (char xDirection, char yDirection) {
+		//TODO
+	}
+
+	public void draw () {
+		//TODO
+	}
+
+
+
+}
